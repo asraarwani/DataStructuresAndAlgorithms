0cdaac0f27a59799ad835f1f89194f0d5f67e6f1
asraar <asraar_tjc_2010@hotmail.com>
2022-10-01 09:53:27 +0530

    Minor code changes

--- Others/src/main/java/com/some_domain/www/interviewquestions/BinarySearch.java
+++ Others/src/main/java/com/some_domain/www/interviewquestions/BinarySearch.java
@@ -18,6 +18,14 @@ public class BinarySearch {
             System.out.println(key + " is not present in the array");
         }
         System.out.println("Time complexity is O(Log N)");
+
+
+        index = instance.binarySearchRecursive(array, 0, array.length - 1, key);
+        if (index != -1) {
+            System.out.println(key + " is present in the array at index " + index);
+        } else {
+            System.out.println(key + " is not present in the array");
+        }
     }
 
     private int binarySearch(int[] array, int key) {
@@ -35,5 +43,21 @@ public class BinarySearch {
             }
         }
         return index;
+    }
+
+    private int binarySearchRecursive(int[] array, int start, int end, int key) {
+
+        if (start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+        if (array[mid] == key)
+            return mid;
+        else if (key < array[mid]) {
+            return binarySearchRecursive(array, start, mid - 1, key);
+        } else if (key > array[mid]) {
+            return binarySearchRecursive(array, mid + 1, end, key);
+        }
+        return -1;
     }
 }
