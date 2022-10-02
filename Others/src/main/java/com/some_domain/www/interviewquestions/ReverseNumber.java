0150ad3457600cba6d34d4714f2f8761cd11f09b
asraar <asraar_tjc_2010@hotmail.com>
2022-10-02 12:32:42 +0530

    Reverse digits of a given number

--- Others/src/main/java/com/some_domain/www/interviewquestions/ReverseNumber.java
+++ Others/src/main/java/com/some_domain/www/interviewquestions/ReverseNumber.java
@@ -3,6 +3,27 @@ package com.some_domain.www.interviewquestions;
 /**
  * @author : asraar
  * @date : 02-10-2022 12:19 pm
+ * <p>
+ * Reference : https://www.geeksforgeeks.org/write-a-program-to-reverse-digits-of-a-number/
  */
 public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        ReverseNumber instance = new ReverseNumber();
+        int number = 12345;
+        instance.reverseGivenNumber(number);
+    }
+
+    private void reverseGivenNumber(int number) {
+        int originalNumber = number;
+        int remainder;
+        int reverse = 0;
+        while (number > 0) {
+            remainder = number % 10;
+            number = number / 10;
+            reverse = reverse * 10 + remainder;
+        }
+        System.out.println("Reverse of number " + originalNumber + " is " + reverse);
+    }
 }
